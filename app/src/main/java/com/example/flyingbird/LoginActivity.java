6fc4242116ceca3d5e6b21116e49f3b5d2ab1040
Naziur Rahman Bishal <nrahmanbishal@gmail.com>
2020-12-22 03:55:39 +0600

    login activity done with back button and only layout. next registration activity

--- app/src/main/java/com/example/flyingbird/LoginActivity.java
+++ app/src/main/java/com/example/flyingbird/LoginActivity.java
@@ -2,13 +2,32 @@ package com.example.flyingbird;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void loginBackPressed(View view) {
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
